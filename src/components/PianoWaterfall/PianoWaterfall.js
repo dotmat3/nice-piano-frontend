5f9a6de36474da17331aa86e92a6000d6ffb0c9a
Fabrizio <fabry_98@hotmail.it>
2021-06-01 18:56:01 +0200

    Improved PianoWaterfall performance

--- src/components/PianoWaterfall/PianoWaterfall.js
+++ src/components/PianoWaterfall/PianoWaterfall.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useRef } from "react";
 
 import { BASES } from "../../constants";
 import { getNoteFromMidiNumber } from "../../utils";
@@ -8,10 +8,8 @@ import "./PianoWaterfall.scss";
 const drawNote = (ctx, x, y, height, width, color) => {
   ctx.strokeStyle = color;
   ctx.lineWidth = width;
-  // ctx.lineCap = "round";
+  ctx.lineCap = "round";
   ctx.beginPath();
-  // ctx.moveTo(x, y + width / 2);
-  // ctx.lineTo(x + width / 2, y + height - width / 2);
   ctx.moveTo(x + width / 2, y);
   ctx.lineTo(x + width / 2, y + height);
   ctx.stroke();
@@ -23,19 +21,20 @@ const CANVAS_HEIGHT = 400;
 const SLOW_FACTOR = 0.2;
 
 const KEYBOARD_KEYS = 9 * 7;
-const MAX_PITCH = 119;
-const MIN_PITCH = 12;
 
 const PianoWaterfall = ({ drawedNotes, removeNote }) => {
+  const notesRef = useRef(null);
+  notesRef.current = drawedNotes;
+
   const draw = useCallback(() => {
     const canvas = document.querySelector("canvas");
     if (!canvas) return;
 
     const ctx = canvas.getContext("2d");
 
-    ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (const [index, note] of Object.entries(drawedNotes)) {
+    for (const [index, note] of Object.entries(notesRef.current)) {
       // pitch : 107 = x : offsetWidth
       const noteStr = getNoteFromMidiNumber(note.pitch);
       let pitch = parseInt(noteStr.substr(noteStr.length - 1, 1)) * 7;
@@ -46,11 +45,12 @@ const PianoWaterfall = ({ drawedNotes, removeNote }) => {
         x = ((pitch - 1) * canvas.offsetWidth) / KEYBOARD_KEYS + NOTE_WIDTH / 2;
       else x = (pitch * canvas.offsetWidth) / KEYBOARD_KEYS;
 
-      const millis = (Date.now() - note.created) * SLOW_FACTOR;
+      const now = Date.now();
+      const millis = (now - note.created) * SLOW_FACTOR;
       let y = canvas.offsetHeight - millis;
 
-      const ended = note.ended ? note.ended : Date.now();
-      const end_millis = (Date.now() - ended) * SLOW_FACTOR;
+      const ended = note.ended ? note.ended : now;
+      const end_millis = (now - ended) * SLOW_FACTOR;
       let end_y = canvas.offsetHeight - end_millis;
 
       let height = end_y - y;
@@ -66,21 +66,23 @@ const PianoWaterfall = ({ drawedNotes, removeNote }) => {
         note.color
       );
     }
-  }, [drawedNotes]);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      draw();
-    }, 10);
+    window.requestAnimationFrame(draw);
+  }, [removeNote]);
 
-    return () => clearInterval(interval);
-  }, [draw]);
+  const animationFrameHandle = useRef(null);
 
   useEffect(() => {
     const canvas = document.querySelector("canvas");
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
-  }, []);
+
+    animationFrameHandle.current = window.requestAnimationFrame(draw);
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameHandle.current);
+    };
+  }, [draw]);
 
   return (
     <div className="piano-waterfall">
@@ -90,59 +92,9 @@ const PianoWaterfall = ({ drawedNotes, removeNote }) => {
         style={{
           width: "100%",
           height: "100%",
-          border: "1px solid lightgreen",
         }}
       />
-      {/* {[...Array(octaves)].map((octave, index) => (
-        <PianoWaterfallOctave key={index} number={octave} notes={notes} />
-      ))} */}
     </div>
-  );
-};
-
-const PianoWaterfallOctave = ({ number, notes }) => {
-  const naturalNotes = Object.keys(BASES);
-
-  return (
-    <div className="piano-waterfall-octave">
-      <div className="piano-waterfall-octave-black">
-        <PianoWaterfallColumn half />
-        {naturalNotes.map((value) => (
-          <PianoWaterfallColumn
-            key={value + "b" + number}
-            note={value + "b" + number}
-          />
-        ))}
-        <PianoWaterfallColumn half />
-      </div>
-      <div className="piano-waterfall-octave-white">
-        {naturalNotes.map((value) => (
-          <PianoWaterfallColumn key={value + number} note={value + number} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-const PianoWaterfallColumn = ({ note, half }) => {
-  if (half) return <div className="piano-waterfall-column hidden half" />;
-
-  if (note.includes("Fb"))
-    return <div className="piano-waterfall-column hidden" />;
-
-  return (
-    <div className="piano-waterfall-column">
-      {<PianoWaterfallNote start={Math.random() * 100} duration={20} />}
-    </div>
-  );
-};
-
-const PianoWaterfallNote = ({ color, start, duration }) => {
-  return (
-    <div
-      className="piano-waterfall-note"
-      style={{ backgroundColor: color, top: start, height: duration }}
-    />
   );
 };
 
